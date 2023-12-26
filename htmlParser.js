1b0568d8599909ff301eafb99ec962b9e21d71df
GerganaRoeva <gergana.roeva@gmail.com>
2023-12-26 17:53:57 +0200

    tokenize elements

---
+++ htmlParser.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+
+class Node {
+  constructor(type, tagName = null, attributes = {}, children = []) {
+    this.type = type;
+    this.tagName = tagName;
+    this.attributes = attributes;
+    this.children = children;
+  }
+}
+
+function parseHTML(html) {
+  const root = new Node("element", "root");
+  const stack = [root];
+  let currentText = "";
+
+  for (let i = 0; i < html.length; i++) {
+    if (html[i] === "<") {
+      if (currentText.trim()) {
+        stack[stack.length - 1].children.push(
+          new Node("text", null, {}, currentText)
+        );
+      }
+      currentText = "";
+      if (html[i + 1] === "/") {
+        let tagName = "";
+        i += 2;
+        while (html[i] !== ">") {
+          tagName += html[i];
+          i++;
+        }
+        stack.pop();
+      } else {
+        let tagName = "";
+        i++;
+        while (html[i] !== ">" && html[i] !== " ") {
+          tagName += html[i];
+          i++;
+        }
+        const newNode = new Node("element", tagName);
+        stack[stack.length - 1].children.push(newNode);
+        stack.push(newNode);
+
+        while (html[i] !== ">") i++;
+      }
+    } else {
+      currentText += html[i];
+    }
+  }
+
+  return root;
+}
+
+function printNode(node, indent = 0) {
+  const indentation = " ".repeat(indent);
+  if (node.type === "text") {
+    console.log(
+      `${indentation}Node: ${node.type}, Content: "${node.children}"`
+    );
+  } else {
+    console.log(`${indentation}Node: ${node.type}, Tag: ${node.tagName}`);
+    node.children.forEach((child) => {
+      printNode(child, indent + 2);
+    });
+  }
+}
+
+function readHTMLFile(filePath) {
+  try {
+    return fs.readFileSync(filePath, "utf-8");
+  } catch (error) {
+    console.error(`Error reading file: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+const filePath = "example.html";
+const htmlContent = readHTMLFile(filePath);
+
+const parsedHTML = parseHTML(htmlContent);
+printNode(parsedHTML);
